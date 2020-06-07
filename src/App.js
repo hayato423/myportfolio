99f2a36634873d2b63aa1ff4c251fd5309560b1f
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-08 00:04:32 +0900

    add shadow at navber

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './componensts/Header/Header';
+import Navbar from './componensts/NavigationBar/NavigationBar';
 import Profile from './componensts/Profile/Profile';
 import Skils from './componensts/Skils/Skils';
 import Works from './componensts/Works/Works';
@@ -10,6 +11,7 @@ import Footer from './componensts/Footer/Footer';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Header/>
       <Profile/>
       <Skils/>
---
+++ src/componensts/NavigationBar/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Navbar,Nav} from 'react-bootstrap';
+import './style.css';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg='light' variant='light' expand='lg' fixed='top' className='navbar'>
+
+    </Navbar>
+  )
+}
+
+export default NavigationBar
