96d18fad335f9a2ef7ec2cf446c81fa8b0d49bef
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-07 13:02:02 +0900

    change email addres

--- src/componensts/Footer/Footer.js
+++ src/componensts/Footer/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './style.css';
-import {Container,Row,Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 const Footer = () => {
   return (
     <div className='footer'>
-      <h2 className='footer-head-line'>Contact</h2>
+      <h2 className='footer-head-line'>Link</h2>
       <Container>
       <a href='https://twitter.com/hayat999423'target="_blank" rel='noopener noreferrer'><i className="fab fa-twitter fa-2x mx-3 mt-3 icon"></i></a>
       <a href='https://github.com/hayato423' target="_blank" rel='noopener noreferrer'><i className="fab fa-github fa-2x mx-3 mt-3 icon"></i></a>
       <h3 className='my-3'>e-mail</h3>
-      <p>hayato0423terao@gmail.com</p>
+      <p>hayato0423terao[at]gmail.com</p>
       </Container>
     </div>
   )
