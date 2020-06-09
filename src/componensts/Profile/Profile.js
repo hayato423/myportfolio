5c4393248dbec80447a2afaa9a039bfac1ed9e40
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-06-09 10:13:01 +0900

    add profile icon

--- src/componensts/Profile/Profile.js
+++ src/componensts/Profile/Profile.js
@@ -1,37 +1,52 @@
 import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import icon from './icon.jpg';
 import './Profile.css';
 
 const Profile = () => {
   return (
-    <div className='container py-5' id='profile'>
-        <h1 className='profile-title'><span className='profile-underline'>Profile</span></h1>
-        <table className='profile-table mx-auto mt-5'>
-          <tbody>
-            <tr>
-              <th >Name</th>
-              <td>寺尾颯人</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th>Univercity</th>
-              <td>芝浦工業大学 工学部 情報工学科</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th>Birthday</th>
-              <td>1999/04/23</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th>Club</th>
-              <td><a href='https://shibalab.com/' className='profile-link' target="_blank" rel='noopener noreferrer'>ShibaLab</a></td>
-            </tr>
-          </tbody>
-        </table>
-    </div>
+    <Container className='py-5' id='profile'>
+      <Row>
+        <Col>
+          <h1 className='profile-title'><span className='profile-underline'>Profile</span></h1>
+        </Col>
+      </Row>
+      <Row className='mt-5'>
+        <Col className='mx-auto'>
+          <Image src={icon} roundedCircle className='fluid img' />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='mt-5'>
+          <table className='profile-table mx-auto'>
+            <tbody>
+              <tr>
+                <th >Name</th>
+                <td>寺尾颯人</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <th>Univercity</th>
+                <td>芝浦工業大学 工学部 情報工学科</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <th>Birthday</th>
+                <td>1999/04/23</td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <th>Club</th>
+                <td><a href='https://shibalab.com/' className='profile-link' target="_blank" rel='noopener noreferrer'>ShibaLab</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
