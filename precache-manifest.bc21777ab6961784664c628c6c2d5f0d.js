b6a004067275984b0e999607673bfe845e42c668
hayato423 <49159244+hayato423@users.noreply.github.com>
2020-11-10 19:28:57 +0900

    Updates

---
+++ precache-manifest.bc21777ab6961784664c628c6c2d5f0d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a00fca0db920c2f0de81baa195131a7",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "3450c7f798e4631bb06c",
+    "url": "/myportfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76b76c11e22145af3f71",
+    "url": "/myportfolio/static/css/main.2890438a.chunk.css"
+  },
+  {
+    "revision": "3450c7f798e4631bb06c",
+    "url": "/myportfolio/static/js/2.bd9e8f65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myportfolio/static/js/2.bd9e8f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b76c11e22145af3f71",
+    "url": "/myportfolio/static/js/main.979bd648.chunk.js"
+  },
+  {
+    "revision": "aa4e5751b3d17ed149e8",
+    "url": "/myportfolio/static/js/runtime-main.27ac374a.js"
+  },
+  {
+    "revision": "60a6cc432237f8859973478202484978",
+    "url": "/myportfolio/static/media/MagicCircle.60a6cc43.mp4"
+  },
+  {
+    "revision": "33d30d1c8a657f7012a87a9a117e975b",
+    "url": "/myportfolio/static/media/bucketlistmaker.33d30d1c.png"
+  },
+  {
+    "revision": "ba689a238392eb7b166f3b9419d6af79",
+    "url": "/myportfolio/static/media/digital_splay2.ba689a23.gif"
+  },
+  {
+    "revision": "0ce548a109312e5222d68d63638f7747",
+    "url": "/myportfolio/static/media/digital_spray.0ce548a1.jpg"
+  },
+  {
+    "revision": "3149e17edd1a4481eb930e0602aa84dc",
+    "url": "/myportfolio/static/media/drunk_checker.3149e17e.png"
+  },
+  {
+    "revision": "291253865e0f7fcdf1dee274f01f8b8e",
+    "url": "/myportfolio/static/media/header.29125386.jpg"
+  },
+  {
+    "revision": "2b1591d681c8731931321628ddaab370",
+    "url": "/myportfolio/static/media/icon.2b1591d6.jpg"
+  },
+  {
+    "revision": "70f82e5f4f5370385f5caff5486906ac",
+    "url": "/myportfolio/static/media/magic_circle.70f82e5f.png"
+  },
+  {
+    "revision": "42f2a318a1a7d93c1466d765365e94b6",
+    "url": "/myportfolio/static/media/mykakeibo.42f2a318.png"
+  },
+  {
+    "revision": "6f949ee4f1c39bbcdea38e3ff070921a",
+    "url": "/myportfolio/static/media/portfolio.6f949ee4.png"
+  },
+  {
+    "revision": "58cd7dda333dd5d5b2dde45c7359f989",
+    "url": "/myportfolio/static/media/srendipity.58cd7dda.jpg"
+  },
+  {
+    "revision": "af30a54853d813714bb4ab0c7fe004b4",
+    "url": "/myportfolio/static/media/tansan2.af30a548.gif"
+  },
+  {
+    "revision": "4cb154d95042db543e8e1007b6240852",
+    "url": "/myportfolio/static/media/tansan_splash.4cb154d9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myportfolio/precache-manifest.2c8108abd9170fa2125f261016ed85fd.js"
+  "/myportfolio/precache-manifest.bc21777ab6961784664c628c6c2d5f0d.js"
 );
 
 self.addEventListener('message', (event) => {
